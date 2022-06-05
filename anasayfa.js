a1c39a9996dd322b086141df9059306c6dfc2526
Ali Mert UYUMAZ <ali.mert1320@gmail.com>
2022-06-05 20:38:03 +0300

    v4

---
+++ anasayfa.js
@@ -0,0 +1,17 @@
+(function() {
+
+    window.addEventListener("load", init);
+    function init() {
+        window.alert("Şu anda yanlış sayfadasınız. Lütfen aşığıdaki yönlendirmeleri uygulayın.");
+      let Button = document.getElementById("btn");
+      Button.addEventListener("click", sayfaDegis);
+    }
+
+
+    function sayfaDegis() {
+       location.href = "anasayfa.html";                        
+    }
+       
+
+  })();
+  
